cb7cadf6ed0995b0a715997ea547bf9de394c9ee
Gabriel Ruiu <gabriel.ruiu@1und1.de>
2017-01-27 19:01:28 +0100

    add main method

--- src/main/java/com/github/gabrielruiu/SampleApp.java
+++ src/main/java/com/github/gabrielruiu/SampleApp.java
@@ -1,15 +1,20 @@
 package com.github.gabrielruiu;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ServletInitializer extends SpringBootServletInitializer {
+public class SampleApp extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApp.class, args);
+    }
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(ServletInitializer.class);
+		return application.sources(SampleApp.class);
 	}
 
 }
